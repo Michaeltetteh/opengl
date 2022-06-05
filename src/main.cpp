0b8c438c9181fab0ab745d0e094b048246b450d4
mike <miketetteh751@outlook.com>
2022-06-05 22:27:42 +0000

    creating a window

--- src/main.cpp
+++ src/main.cpp
@@ -42,8 +42,11 @@ int main()
     {
         processInput(window);
 
-        glfwSwapBuffers(window);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwPollEvents();
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();
