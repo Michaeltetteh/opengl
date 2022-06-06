15ecc807b735efcf507c45582c6ae953e3a7856c
mike <miketetteh751@outlook.com>
2022-06-06 16:46:31 +0000

    added shaders

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include "../include/glad/glad.h"
+#include <string>
+#include <algorithm>
 
 void frame_buffer_size_callback(GLFWwindow *window,int w,int h)
 {
@@ -11,6 +13,52 @@ void processInput(GLFWwindow *window)
     if(glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window,true);
 }
+
+void check_shader_compilation_errors(GLuint shader,std::string shader_type)
+{
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader,GL_COMPILE_STATUS,&success);
+    if(!success)
+    {
+        glGetShaderInfoLog(shader,512,NULL,infoLog);
+        std::transform(shader_type.begin(),shader_type.end(),shader_type.begin(),::toupper);
+        std::cout << "ERROR::SHADER::"<<shader_type<<"::COMPILATION_FAILED\n" << infoLog << "\n";
+    }
+}
+
+void check_program_compilation_error(GLuint program)
+{
+    int success;
+    char infoLog[512];
+    glGetProgramiv(program,GL_LINK_STATUS,&success);
+    if(!success)
+    {
+        glGetProgramInfoLog(program,512,NULL,infoLog);
+        std::cout << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << "\n";
+    }
+}
+
+float vertices[] ={
+        -0.5f,-0.5f,0.0f,
+        0.5f,-0.5f,0.0f,
+        0.0f,0.5f,0.0f
+};
+
+
+const char *vertex_shader =
+        "#version 330 core \n"
+        "layout (location=0) in vec3 pos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(pos.x,pos.y,pos.z, 1.0);\n"
+        "}\n";
+const char *fragment_shader =
+        "#version 330 core\n"
+        "out vec4 fragColor;\n"
+        "void main(){\n"
+        " fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n";
 
 int main()
 {
@@ -34,6 +82,46 @@ int main()
         std::cout<<"Failed to initialized glad"<<"\n";
         return -1;
     }
+
+    unsigned int VBO,vertexShader,fragmentShader,shaderProgram;
+
+    //creating memory in the GPU for the vertex data to be stored
+    glGenBuffers(1,&VBO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+
+    //dynamically compiling the shader
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    //attach shader source to the shader object and compile
+    glShaderSource(vertexShader,1,&vertex_shader,NULL);
+    glCompileShader(vertexShader);
+
+    //check for errors in vertex shader compilation if any
+    check_shader_compilation_errors(vertexShader,"vertex");
+
+    //compile fragment shader
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader,1,&fragment_shader,NULL);
+    glCompileShader(fragmentShader);
+
+    //check for fragment shader compilation errors
+    check_shader_compilation_errors(fragmentShader,"fragment");
+
+    //linking shader to a shader program
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram,vertexShader);
+    glAttachShader(shaderProgram,fragmentShader);
+    glLinkProgram(shaderProgram);
+    //check program link errors
+    check_program_compilation_error(shaderProgram);
+
+    //delete shader objects
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    //use program
+    glUseProgram(shaderProgram);
+
 
 //    glViewport(0,0,800,600);
     glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
