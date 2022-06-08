b36c7ef760dd5c2e6f9f0f1afc1363eedbf7afda
mike <miketetteh751@outlook.com>
2022-06-08 16:19:52 +0000

    vertex attributtes

--- src/shaders.cpp
+++ src/shaders.cpp
@@ -44,17 +44,20 @@ void check_program_compilation_error(GLuint program)
 const char *vertex_shader =
         "#version 330 core \n"
         "layout (location=0) in vec3 pos;\n"
+        "layout (location = 1) in vec3 aColor;\n"
+        "out vec3 ourColor;\n"
         "void main()\n"
         "{\n"
         "   gl_Position = vec4(pos, 1.0);\n"
+        "   ourColor = aColor;\n"
         "}\n";
 
 const char *fragment_shader =
         "#version 330 core\n"
         "out vec4 fragColor;\n"
-        "uniform vec4 ourColor;\n"
+        "in vec3 ourColor;\n"
         "void main(){\n"
-        " fragColor = ourColor;\n"
+        " fragColor = vec4(ourColor,1.0);\n"
         "}\n";
 
 
@@ -121,9 +124,9 @@ int main()
 
 
     float vertices[] ={
-            -0.2f,-0.2f,0.0f,
-            0.2f,-0.2f,0.0f,
-            0.0f,0.2f,0.0f
+            -0.2f,-0.2f,0.0f,1.0f,0.0f,0.0f,
+            0.2f,-0.2f,0.0f,0.0f,1.0f,0.0f,
+            0.0f,0.2f,0.0f,0.0f,0.0f,1.0f
     };
 
 
@@ -137,8 +140,18 @@ int main()
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
 
     //linking vertex attributes
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
-    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0); //enable vertex attrib 0(location =0)
+
+    glVertexAttribPointer(
+            1,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            6*sizeof(float),
+            (void*)(3*sizeof(float)) //starting position  in the vertices array [3]
+    );
+    glEnableVertexAttribArray(1); //vertex attrib 1
 
 
 
@@ -157,13 +170,13 @@ int main()
         glUseProgram(shaderProgram);
 
         //color gradient
-        float timeVal = glfwGetTime();
-        float greenValue = (sin(timeVal) /2.0f) + 0.5f;
+//        float timeVal = glfwGetTime();
+//        float greenValue = (sin(timeVal) /2.0f) + 0.5f;
         //std::cout<<"Time(s): "<<timeVal<<" greenval: "<<greenValue<<"Sine(t): "<< sin(timeVal)<<"\n";
-        int vertexColorLocation = glGetUniformLocation(shaderProgram,"ourColor");
+//        int vertexColorLocation = glGetUniformLocation(shaderProgram,"ourColor");
 
         //                  uniform location        R          G           B          A
-        glUniform4f(vertexColorLocation,0.0f,greenValue,0.0f,1.0f);
+        //glUniform4f(vertexColorLocation,0.0f,greenValue,0.0f,1.0f);
 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES,0,3);
