fb5fc8c612181d587a98bf65b0a7edd8e7723bcc
mike <miketetteh751@outlook.com>
2022-06-09 14:28:48 +0000

    added texture

--- src/textures.cpp
+++ src/textures.cpp
@@ -14,11 +14,11 @@ int main()
     );
 
     float vertices[] ={
-            //          positions          ||            colors          ||     texture coordinates
-            0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,        // top right
-            0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,    // bottom right
+            //          positions          ||            colors             ||  texture coordinates
+            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f, 1.0f, 1.0f,        // top right
+            0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f, 0.0f,    // bottom right
             -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
-            -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f // top left
+            -0.5f,  0.5f, 0.0f,1.0f, 1.0f, 1.0f, 0.0f, 1.0f // top left
     };
 
     unsigned int indices[] = {
