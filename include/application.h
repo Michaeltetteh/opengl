d4448d5296d0fddeeb24dc0799f2513c7cbdf379
mike <miketetteh751@outlook.com>
2022-06-09 11:48:17 +0000

    application header added

---
+++ include/application.h
@@ -0,0 +1,60 @@
+#ifndef OPENGL_APPLICATION_H
+#define OPENGL_APPLICATION_H
+#include <GLFW/glfw3.h>
+#include "../include/glad/glad.h"
+#include <iostream>
+#include <string>
+
+/**
+ * Sets up opengl application(creates window(s) and initializes glad
+ *
+ * */
+class Application
+{
+public:
+    Application(int width,int height,std::string title);
+    ~Application();
+
+    static void frame_buffer_size_callback(GLFWwindow *window,int w,int h);
+
+    GLFWwindow *window;
+
+};
+
+#endif //OPENGL_APPLICATION_H
+
+
+Application::Application(int width, int height,std::string title="OpenGl Window")
+{
+
+    //glfw window setup
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);
+
+    window = glfwCreateWindow(width,height,title.c_str(),NULL,NULL);
+    if(window == NULL)
+    {
+        std::cout<<"Failed to create window"<<"\n";
+        glfwTerminate();
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+
+    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout<<"Failed to initialized glad"<<"\n";
+    }
+}
+
+void Application::frame_buffer_size_callback(GLFWwindow *window, int w, int h)
+{
+    glViewport(0,0,w,h);
+}
+
+Application::~Application()
+{
+    glfwTerminate();
+}
---
+++ src/textures.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include "../include/shader.h"
+#include "../include/application.h"
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window,true);
+}
+
+int main()
+{
+    Application app(800,600,"App Window");
+
+    Shader shader(
+            "/Users/mike/Desktop/cpp_projects/opengl/src/shaders/vertex_shader.shader",
+            "/Users/mike/Desktop/cpp_projects/opengl/src/shaders/fragment_shader.shader"
+    );
+
+
+    float vertices[] ={
+            -0.2f,-0.2f,0.0f,1.0f,0.0f,0.0f,
+            0.2f,-0.2f,0.0f,0.0f,1.0f,0.0f,
+            0.0f,0.2f,0.0f,0.0f,0.0f,1.0f
+    };
+
+
+    //creating and binding vertex array object
+    unsigned int VAO,VBO;
+    glGenVertexArrays(1,&VAO);
+    glBindVertexArray(VAO);
+    //creating memory in the GPU for the vertex data to be stored
+    glGenBuffers(1,&VBO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+
+    //linking vertex attributes
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0); //enable vertex attrib 0(location =0)
+
+    glVertexAttribPointer(
+            1,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            6*sizeof(float),
+            (void*)(3*sizeof(float)) //starting position  in the vertices array [3]
+    );
+    glEnableVertexAttribArray(1); //vertex attrib 1
+
+
+
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindVertexArray(0);
+
+    while (!glfwWindowShouldClose(app.window))
+    {
+        processInput(app.window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //use program
+        shader.use();
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES,0,3);
+
+        glfwSwapBuffers(app.window);
+        glfwPollEvents();
+
+    }
+
+    //deallocate all resources
+    glDeleteProgram(shader.getProgram());
+    glDeleteBuffers(1,&VBO);
+    glDeleteVertexArrays(1,&VAO);
+
+
+
+    return 0;
+}
