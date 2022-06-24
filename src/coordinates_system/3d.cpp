3a9394f319b15cb9879930ec62d8edd8627bfa7c
mike <miketetteh751@outlook.com>
2022-06-24 16:12:59 +0000

    coordinate system

--- src/coordinates_system/3d.cpp
+++ src/coordinates_system/3d.cpp
@@ -167,8 +167,7 @@ int main()
         glm::mat4 view = glm::mat4 (1.0f);
         glm::mat4 projection = glm::mat4 (1.0f);
 
-        model = glm::rotate(model, (float)glfwGetTime()* glm::radians(50.0f),
-                            glm::vec3(0.5f, 1.0f, 0.0f));
+        model = glm::rotate(model, (float)glfwGetTime(),glm::vec3(0.5f, 1.0f, 0.0f));
         view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 0.1f,
                                       100.0f);
