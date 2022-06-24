851f60cae1a623e8a3b6b25613be52759b102dcd
mike <miketetteh751@outlook.com>
2022-06-24 16:46:04 +0000

    coordinate system

--- src/coordinates_system/3d.cpp
+++ src/coordinates_system/3d.cpp
@@ -51,6 +51,19 @@ float vertices[] = {
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
+glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,   0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f,  -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f,  -3.5f),
+        glm::vec3(-1.7f,  3.0f,  -7.5f),
+        glm::vec3( 1.3f, -2.0f,  -2.5f),
+        glm::vec3( 1.5f,  2.0f,  -2.5f),
+        glm::vec3( 1.5f,  0.2f,  -1.5f),
+        glm::vec3(-1.3f,  1.0f,  -1.5f)
+    };
+
 int main()
 {
     Application app(800,600,"Transformation");
@@ -163,23 +176,33 @@ int main()
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
-        glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 view = glm::mat4 (1.0f);
         glm::mat4 projection = glm::mat4 (1.0f);
 
-        model = glm::rotate(model, (float)glfwGetTime(),glm::vec3(0.5f, 1.0f, 0.0f));
         view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 0.1f,
+        projection = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 1.0f,
                                       100.0f);
 
         //unsigned int uniformlocation = glGetUniformLocation(shader.getProgram(), "model");
         //glUniformMatrix4fv(uniformlocation,1,GL_FALSE,glm::value_ptr(model));
-        shader.setMat4("model",model);
         shader.setMat4("view",view);
         shader.setMat4("projection",projection);
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES,0,36);
+        for(unsigned int i=0;i<10;i++)
+        {
+            float angle = 0.0f;
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model,cubePositions[i]);
+            if(i > 0)
+                angle = 40.0f * i;
+            else
+                angle = 20.0f;
+            model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle),
+                                glm::vec3(0.3f, 0.3f, 0.5f));
+            shader.setMat4("model",model);
+            glDrawArrays(GL_TRIANGLES,0,36);
+        }
 
 
         glfwSwapBuffers(app.window);
