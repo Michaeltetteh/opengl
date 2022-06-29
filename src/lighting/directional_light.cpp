d57945c39c160e573c9ef05c9461c60fd05fe58b
mike <miketetteh751@outlook.com>
2022-06-29 13:59:18 +0000

    directional caster light

---
+++ src/lighting/directional_light.cpp
@@ -0,0 +1,199 @@
+#include <GLFW/glfw3.h>
+#include "../../include/shader.h"
+#include "../../include/application.h"
+#include "../../include/stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "../../include/utils.h"
+//#include <glm/gtx/string_cast.hpp>
+
+float vertices[] = {
+        // positions          // normals           // texture coords
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+        0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+        0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
+};
+
+glm::vec3 cubePositions[] = {
+        glm::vec3( 0.3f,  0.0f,   0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f,  -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f,  -3.5f),
+        glm::vec3(-1.7f,  3.0f,  -7.5f),
+        glm::vec3( 1.3f, -2.0f,  -2.5f),
+        glm::vec3( 1.5f,  2.0f,  -2.5f),
+        glm::vec3( 1.5f,  0.2f,  -1.5f),
+        glm::vec3(-1.3f,  1.0f,  -1.5f)
+};
+
+glm::vec3 LIGHTCOLOR = glm::vec3(1.0f);
+glm::vec3 lightPos = glm::vec3(1.2f, 1.0f, 2.0f);
+
+
+int main()
+{
+    Application app("Lighting");
+
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile shader program
+    // ------------------------------------
+    Shader cubeShader("shaders/directional_light/cubeVertex.shader","shaders/directional_light/cubeFrag.shader");
+    Shader lightShader("shaders/directional_light/lightVertex.shader","shaders/directional_light/lightFrag.shader");
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    unsigned int VBO, CUBEVAO,LIGHTSRCVAO;
+    glGenBuffers(1, &VBO);
+
+    // cube VAO
+    glGenVertexArrays(1, &CUBEVAO);
+    glBindVertexArray(CUBEVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // vertex attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,8 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
+    //normals
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    //texture coordinates
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    //light VAO
+    glGenVertexArrays(1, &LIGHTSRCVAO);
+    glBindVertexArray(LIGHTSRCVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,8 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
+
+    //use texture
+    cubeShader.use();
+    unsigned int diffuseMap = loadTexture("resources/textures/container2.png");
+    unsigned int specularMap = loadTexture("resources/textures/container2_specular.png");
+    cubeShader.setInt("material.diffuse",0);
+    cubeShader.setInt("material.specular",1);
+
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(app.window))
+    {
+        // input
+        // -----
+        auto currentFrame = static_cast<float>(glfwGetTime());
+        app.deltaTime = currentFrame - app.lastFrame;
+        app.lastFrame = currentFrame;
+
+        app.processCameraInput();
+
+        // render
+        // ------
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        //CUBE
+        cubeShader.use();
+        glm::mat4 projection = glm::mat4(1.0f);
+        projection = glm::perspective(glm::radians(Application::camera.Zoom), (float)800 / (float)600, 1.0f,100.0f);
+        glm::mat4 view = Application::camera.GetViewMatrix();
+
+        cubeShader.setMat4("view",view);
+        cubeShader.setMat4("projection",projection);
+        cubeShader.setVec3("ViewPosition",Application::camera.Position);
+        cubeShader.setVec3("light.ambient",glm::vec3(0.2f));
+        cubeShader.setVec3("light.diffuse",glm::vec3(0.3f));
+        cubeShader.setVec3("light.specular",glm::vec3(1.0f));
+        cubeShader.setVec3("light.direction",glm::vec3(-0.2f, -1.0f, -0.3f));
+        cubeShader.setFloat("material.shininess", 64.0f);
+
+        glBindVertexArray(CUBEVAO);
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            model = glm::rotate(model,(float)glfwGetTime() * glm::radians(40.0f),glm::vec3(0.0f,1.0f,0.0f));
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle),
+                                glm::vec3(1.0f, 0.3f, 0.5f));
+            cubeShader.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, diffuseMap);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, specularMap);
+
+
+        //Light source
+//        lightShader.use();
+//        glm::mat4 model(1.0f);
+//        model = glm::mat4(1.0f);
+//        model = glm::translate(model, lightPos);
+//        model = glm::scale(model, glm::vec3(0.2f));
+//        lightShader.setMat4("view",view);
+//        lightShader.setMat4("projection",projection);
+//        lightShader.setVec3("LColor",LIGHTCOLOR);
+//        //lightShader.setVec3("LColor",diffuseColor);
+//        lightShader.setMat4("model",model);
+//
+//        glBindVertexArray(LIGHTSRCVAO);
+//        glDrawArrays(GL_TRIANGLES,0,36);
+
+
+        glfwSwapBuffers(app.window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &CUBEVAO);
+    glDeleteVertexArrays(1, &LIGHTSRCVAO);
+    glDeleteBuffers(1, &VBO);
+
+    return 0;
+}
