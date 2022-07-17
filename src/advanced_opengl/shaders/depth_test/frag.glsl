f49cbfba2ba116ca005342ceb51bf0433577ff91
mike <miketetteh751@outlook.com>
2022-07-17 17:01:28 +0000

    visualizing depth buffer

--- src/advanced_opengl/shaders/depth_test/frag.glsl
+++ src/advanced_opengl/shaders/depth_test/frag.glsl
@@ -5,7 +5,20 @@ in vec2 TextureCoordinate;
 
 uniform sampler2D texture1;
 
+float near = 0.1f;
+float far = 100.0f;
+
+float linearizedDepth(float depth)
+{
+    float ndc_z = depth * 2.0 - 1.0;
+    return (2.0 * near * far) / (far + near - ndc_z * (far - near));
+}
+
 void main()
 {
-    FragColor = texture(texture1, TextureCoordinate);
+    //FragColor = texture(texture1, TextureCoordinate);
+    //FragColor = vec4(vec3(gl_FragCoord.z),1.0f);
+    float depth = linearizedDepth(gl_FragCoord.z) / far;
+    FragColor = vec4(vec3(depth),1.0f);
+
 }
