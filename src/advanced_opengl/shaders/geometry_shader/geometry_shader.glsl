320319b3039a24adfc5087b1525d9346ea57487f
mike <miketetteh751@outlook.com>
2023-03-26 17:37:39 +0000

    geometry shader

--- src/advanced_opengl/shaders/geometry_shader/geometry_shader.glsl
+++ src/advanced_opengl/shaders/geometry_shader/geometry_shader.glsl
@@ -1,15 +1,37 @@
 #version 330 core
 
 layout (points) in;
-layout (triangle_strip, max_vertices=3) out;
+layout (triangle_strip, max_vertices=5) out;
+
+//color interface block
+in VS_OUT
+{
+    vec3 color;
+} gs_in[];
+
+//pass color to fragment shader
+out vec3 fColor;
+
+void draw_house(vec4 position)
+{
+    gl_Position = position + vec4(-0.2, -0.2, 0.0, 0.0); // 1:bottom-left
+    EmitVertex();
+    gl_Position = position + vec4( 0.2, -0.2, 0.0, 0.0); // 2:bottom-right
+    EmitVertex();
+    gl_Position = position + vec4(-0.2, 0.2, 0.0, 0.0);  // 3:top-left
+    EmitVertex();
+    gl_Position = position + vec4( 0.2, 0.2, 0.0, 0.0);  // 4:top-right
+    EmitVertex();
+    gl_Position = position + vec4( 0.0, 0.4, 0.0, 0.0);  // 5:top
+    fColor = vec3(1.0, 1.0, 1.0);
+    EmitVertex();
+    
+    EndPrimitive();
+}
+
 
 void main()
 {
-    gl_Position = gl_in[0].gl_Position + vec4(-0.1, 0.0, 0.0, 0.0);
-    EmitVertex();    
-    gl_Position = gl_in[0].gl_Position + vec4(0.1, 0.0, 0.0, 0.0);
-    EmitVertex();    
-    gl_Position = gl_in[0].gl_Position + vec4(0.0, 0.1, 0.0, 0.0);
-    EmitVertex();
-    EndPrimitive();
+    fColor = gs_in[0].color;
+    draw_house(gl_in[0].gl_Position);
 }
--- src/advanced_opengl/shaders/geometry_shader/vertex_shader.glsl
+++ src/advanced_opengl/shaders/geometry_shader/vertex_shader.glsl
@@ -1,8 +1,17 @@
 #version 330 core
 
 layout (location = 0) in vec2 pos;
+layout (location = 1) in vec3 Color;
+
+//foward color to next shader
+out VS_OUT
+{
+    vec3 color;
+} vs_out;
+
 
 void main()
 {
     gl_Position = vec4(pos.x, pos.y, 0.0, 1.0);
+    vs_out.color = Color;
 }
